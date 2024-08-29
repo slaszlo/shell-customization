8c6317feca4b7a6f92f5c1414f522fc855cea8af
Laszlo Sarkozi - saras <saras8472@gmail.com>
2024-08-29 21:30:27 +0200

    .s_hell: add loader scripts.

---
+++ home/.s_hell/init.sh
@@ -0,0 +1,4 @@
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+source ${SCRIPTPATH}/variables/vars.sh
+source ${SCRIPTPATH}/utils/utils.sh
---
+++ home/.s_hell/utils/utils.sh
@@ -0,0 +1,4 @@
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+source "${SCRIPTPATH}/s_terminal.sh"
+source "${SCRIPTPATH}/s_sync_repos.sh"
